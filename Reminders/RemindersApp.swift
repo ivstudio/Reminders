5d7f79c3ac343bf222a421b3cdcc414c889a7677
Iggy Villamar <iggy@ivstudio.com>
2022-12-08 06:44:14 -0500

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Ignacio Villamar on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
